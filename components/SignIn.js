cf758705782a79c31f68fa16d6d5b5c62119ad32
Audrey Seignol <aseignol@yahoo.fr>
2025-02-26 15:24:36 +0100

    add fetch for signin and signup

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,16 +1,37 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
+
 
 function SignIn(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        fetch('http://localhost:3000/users/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    setEmail('');
+                    setPassword('');
+                } else {
+                    alert(data.error);
+                }
+            })
+    }
+
     return (
         <div className={styles.mainContent}>
             <h1 className={styles.title}>Welcome to BookClubBuddy</h1>
             <h2 className={styles.subtitle}>Gère ton propre club de lecture ou bien suis les dernières actualités livresques de tes clubs de lecture préférés.</h2>
             <label for="email" className={styles.label}>Email</label>
-            <input type="email" id="email" name="email" className={styles.input} placeholder='example@gmail.com' />
+            <input type="email" id="email" name="email" className={styles.input} placeholder='example@gmail.com' onChange={(e) => setEmail(e.target.value)} value={email}/>
             <label for="mdp" className={styles.label}>Mot de passe</label>
-            <input type="password" id="mdp" name="mdp" className={styles.input} placeholder='mot de passe' />
-            <button className={styles.buttonConnect}>Se connecter</button>
+            <input type="password" id="mdp" name="mdp" className={styles.input} placeholder='mot de passe' onChange={(e) => setPassword(e.target.value)} value={password}/>
+            <button className={styles.buttonConnect} onClick={handleSubmit}>Se connecter</button>
             <hr />
             <button className={styles.buttonConnect}>Se connecter avec Google</button>
             <span className={styles.create}>Pas de compte? <a href="#" onClick={(e) => { e.preventDefault(); props.onSwitch(); }}>Se créer un compte.</a></span>
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 function SignUp(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [pseudo, setPseudo] = useState('');
+
+
+    const handleSubmit = () => {
+        fetch('http://localhost:3000/users/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ pseudo, email, password }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    setEmail('');
+                    setPseudo('');
+                    setPassword('');
+                } else {
+                    alert(data.error);
+                }
+            })
+    };
+
     return (
         <div className={styles.mainContent}>
             <h1 className={styles.title}>Welcome to BookClubBuddy</h1>
             <h2 className={styles.subtitle}>Gère ton propre club de lecture ou bien suis les dernières actualités livresques de tes clubs de lecture préférés.</h2>
             <label for="pseudo" className={styles.label}>Pseudo</label>
-            <input type="text" id="pseudo" name="pseudo" className={styles.input} placeholder='pseudo' />
+            <input type="text" id="pseudo" name="pseudo" className={styles.input} placeholder='pseudo' onChange={(e) => setPseudo(e.target.value)} value={pseudo} />
             <label for="email" className={styles.label}>Email</label>
-            <input type="email" id="email" name="email" className={styles.input} placeholder='example@gmail.com' />
+            <input type="email" id="email" name="email" className={styles.input} placeholder='example@gmail.com' onChange={(e) => setEmail(e.target.value)} value={email} />
             <label for="mdp" className={styles.label}>Mot de passe</label>
-            <input type="password" id="mdp" name="mdp" className={styles.input} placeholder='mot de passe' />
-            <button className={styles.buttonConnect}>Créer un compte</button>
+            <input type="password" id="mdp" name="mdp" className={styles.input} placeholder='mot de passe' onChange={(e) => setPassword(e.target.value)} value={password} />
+            <button className={styles.buttonConnect} onClick={handleSubmit}>Créer un compte</button>
             <hr />
-            
+
             <span className={styles.create}>Déjà un compte? <a href="#" onClick={(e) => { e.preventDefault(); props.onSwitch(); }}>Se connecter.</a></span>
         </div>
     );

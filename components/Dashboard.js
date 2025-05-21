e798a87292abc5c1c6183815c63f7108ea30e623
Audrey Seignol <aseignol@yahoo.fr>
2025-05-21 19:42:09 +0200

    Add bookclubs list to dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,23 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import Header from "./Header";
+import styles from '../styles/Dashboard.module.css'
 
 function Dashboard() {
     const user = useSelector((state) => state.user.value);
     const router = useRouter();
+    const [userBookclubs, setUserBookclubs] = useState([]);
+    const [followingBookclubs, setFollowingBookclubs] = useState([]);
 
     useEffect(() => {
         if (!user?.token) {
             router.push('/');
         }
     }, [user, router]);
-    
+
+    const handleIconNew = () => {
+        console.log('New bookclub')
+    };
+
     return (
         <div>
             <Header />
-            Bienvenue {user.pseudo} ! 
-        </div>
+            <div className={styles.container}>
+                Bienvenue {user.pseudo} !
+                <div className={styles.content}>
+                    <div className={styles.subtitle}>
+                        <FontAwesomeIcon icon={faPlus} className={styles.subtitleIcon} onClick={handleIconNew}/><span>Tes clubs de lecture</span>
+                    </div>
+                    {userBookclubs.length === 0 ? "Vous n'avez pas créé de club de lecture" : userBookclubs.map(bk => <img src={bk.icon} alt={bk.name} />)}
+                    <hr></hr>
+                    <div className={styles.subtitle}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.subtitleIcon} /><span>Les clubs de lecture que tu suis</span>
+                    </div>
+                    {followingBookclubs.length === 0 ? "Vous n'avez pas rejoint de club de lecture" : followingBookclubs.map(bk => <img src={bk.icon} alt={bk.name} />)}
+                </div>
+            </div>
+        </div >
     );
 }
 

31ae35b036dcb2286989195d8c4bba1e2f051bd4
Audrey Seignol <aseignol@yahoo.fr>
2025-06-13 11:58:53 +0200

    add search bar for bookclubs

--- components/DashboardBookclubs.js
+++ components/DashboardBookclubs.js
@@ -6,24 +6,57 @@ import { faPlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import styles from '../styles/DashboardBookclubs.module.css'
 import CustomSnackbar from './CustomSnackbar';
 import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Avatar from '@mui/material/Avatar';
+import { styled } from "@mui/material/styles";
 import Link from 'next/link';
 
 function DashboardBookclubs() {
     const user = useSelector((state) => state.user.value);
     const router = useRouter();
     const [userFollow, setUserFollow] = useState([]);
+    const [bookclubs, setBookclubs] = useState([]);
     const [open, setOpen] = useState(false);
     const [name, setName] = useState('');
     const [desc, setDesc] = useState('');
     const [privacy, setPrivacy] = useState(false); // true = private, false = public
     const [icon, setIcon] = useState(null);
     const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [openSearch, setOpenSearch] = useState(false);
+    const [valueSearch, setValueSearch] = useState(null);
 
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const refreshBookclubs = () => {
+    const StyledAutocomplete = styled(Autocomplete)({
+        "&.Mui-focused .MuiInputLabel-outlined": {
+            color: 'var(--primary-color)'
+        },
+        "& .MuiAutocomplete-inputRoot": {
+            color: 'var(--primary-color)'
+        },
+        "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: 'var(--primary-color)'
+        },
+        "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: 'var(--primary-color)'
+        },
+        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: 'var(--primary-color)'
+        },
+        '.MuiAutocomplete-inputFocused': {
+            border: 'none',
+            color: 'var(--primary-color)'
+        },
+        '.MuiFormLabel-root': {
+            color: 'var(--primary-color)'
+        },
+    });
+
+    const refreshFollowings = () => {
         fetch(process.env.NEXT_PUBLIC_API_URL + 'followers/user?userId=', {
             headers: {
                 'authorization': 'Bearer ' + user.token,
@@ -79,7 +112,7 @@ function DashboardBookclubs() {
             setIcon(null);
             setPrivacy(false);
             setOpen(false);
-            refreshBookclubs();
+            refreshFollowings();
             setSnackbarOpen(true);
 
         } catch {
@@ -92,7 +125,23 @@ function DashboardBookclubs() {
             router.push('/');
         }
 
-        refreshBookclubs();
+        fetch(process.env.NEXT_PUBLIC_API_URL + 'bookclubs/all', {
+            headers: {
+                'authorization': 'Bearer ' + user.token,
+            },
+        }).then(res => res.json())
+            .then(data => {
+                setBookclubs(data.bookclubs)
+            })
+
+    }, []);
+
+    useEffect(() => {
+        if (!user?.token) {
+            router.push('/');
+        }
+
+        refreshFollowings();
 
     }, [user, router]);
 
@@ -132,14 +181,40 @@ function DashboardBookclubs() {
             <CustomSnackbar open={snackbarOpen} onClose={() => setSnackbarOpen(false)} msg="Création réussie !" />
 
             <div className={styles.iconsContainer}>
-                {modoBookclubs.length === 0 ? "Tu es modérateur-trice d'aucun club de lecture" : modoBookclubs.map((bk, i) => <div className={styles.bookclubIconDiv} key={i}><Link href={`/bookclub/${bk.id_bookclub._id}`}><img src={bk.id_bookclub.icon} alt={bk.id_bookclub.name} className={styles.bookclubIcon} /></Link></div>)}
+                {modoBookclubs.length === 0 ? "Tu es modérateur-trice d'aucun club de lecture" : 
+                modoBookclubs.map((bk, i) => <div className={styles.bookclubIconDiv} key={i}><Link href={`/bookclub/${bk.id_bookclub._id}`}><img src={bk.id_bookclub.icon} alt={bk.id_bookclub.name} className={styles.bookclubIcon} /></Link></div>)}
             </div>
             <hr />
             <div className={styles.subtitle}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.subtitleIcon} /><span>Les clubs de lecture que tu suis</span>
+                <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.subtitleIcon} onClick={() => setOpenSearch(true)} /><span>Les clubs de lecture que tu suis</span>
             </div>
+
+            <Modal open={openSearch} onClose={() => setOpenSearch(false)}>
+                <div className={styles.modal}>
+                    <h3 className={styles.modalTitle}>Recherche club de lecture</h3>
+                    <div className={styles.inputs}>
+                        <StyledAutocomplete id="select-user" sx={{ width: 1 }}
+                            options={bookclubs.sort((a, b) => -b.name[0].localeCompare(a.name[0]))} autoHighlight disableCloseOnSelect
+                            getOptionLabel={(option) => option.name} value={valueSearch} onChange={(event, newValue) => { setValueSearch(newValue) }}
+                            renderOption={(props, option) => {
+                                const { key, ...optionProps } = props;
+                                return (
+                                    <Box key={key} component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...optionProps}>
+                                        {<Avatar loading="lazy" src={option.icon} alt={option.icon} variant="rounded" sx={{ width: 32, height: 32, mr: 2 }} />}
+                                        {option.name}
+                                    </Box>
+                                );
+                            }}
+                            renderInput={(params) => <TextField {...params} label="Choisir un membre" />}
+                        />
+                        <Link href={`/bookclub/${valueSearch._id}`}><button>Afficher le club de lecture</button></Link>
+                    </div>
+                </div>
+            </Modal>
+
             <div className={styles.iconsContainer}>
-                {followBookclubs.length === 0 ? "Tu n'as pas encore rejoint de club de lecture" : followBookclubs.map((bk, i) => <div className={styles.bookclubIconDiv} key={i}><Link href={`/bookclub/${bk.id_bookclub._id}`}><img src={bk.id_bookclub.icon} alt={bk.id_bookclub.name} className={styles.bookclubIcon} /></Link></div>)}
+                {followBookclubs.length === 0 ? "Tu n'as pas encore rejoint de club de lecture" : 
+                followBookclubs.map((bk, i) => <div className={styles.bookclubIconDiv} key={i}><Link href={`/bookclub/${bk.id_bookclub._id}`}><img src={bk.id_bookclub.icon} alt={bk.id_bookclub.name} className={styles.bookclubIcon} /></Link></div>)}
             </div>
         </div>
 
